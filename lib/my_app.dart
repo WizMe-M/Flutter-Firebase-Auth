8d3db523daaec14c11f1794aeb9eddccf8e26bd2
WizMe-M <timkin.moxim@mail.ru>
2023-03-07 17:31:28 +0300

    routes for app

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,22 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_auth/routing/app_path.dart';
+import 'package:flutter_firebase_auth/widget/page/auth_email_link_page.dart';
+import 'package:flutter_firebase_auth/widget/page/auth_email_password_page.dart';
 import 'package:flutter_firebase_auth/widget/page/auth_page.dart';
+import 'package:flutter_firebase_auth/widget/page/home_page.dart';
 import 'package:flutter_firebase_auth/widget/page/reg_page.dart';
 import 'package:go_router/go_router.dart';
 
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
-  final GoRouter _router = GoRouter(routes: [
-    GoRoute(
-      path: AppPage.signIn.path,
-      builder: (context, state) => AuthPage(),
-    ),
-    GoRoute(
-      path: AppPage.signUp.path,
-      builder: (context, state) => const RegPage(),
-    ),
-  ], initialLocation: AppPage.signIn.path);
+  final GoRouter _router = GoRouter(
+    initialLocation: AppPage.signIn.path,
+    routes: [
+      GoRoute(
+        path: AppPage.signUp.path,
+        builder: (context, state) => const RegPage(),
+      ),
+      GoRoute(
+        path: AppPage.signIn.path,
+        builder: (context, state) => AuthPage(),
+      ),
+      GoRoute(
+        path: AppPage.signInWithPassword.path,
+        builder: (context, state) => const AuthEmailPasswordPage(),
+      ),
+      GoRoute(
+        path: AppPage.signInWithLink.path,
+        builder: (context, state) => const AuthEmailLinkPage(),
+      ),
+      GoRoute(
+        path: AppPage.home.path,
+        builder: (context, state) => HomePage(email: state.params['email']!),
+      )
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
